e7ca5d7539dccea79ec02f51cd7309454aa843c3
박상호 <mozzzi2222@gmail.com>
2023-03-29 17:44:43 +0900

    가보자

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>프로젝트 시작! git client push 꼭하자!!</Text>
-      <Text>집중이 안되네 vscode git check</Text>
+      <Text>집중이 안되네 vscode git checksadfasf</Text>
       <StatusBar style="auto" />
     </View>
   );
